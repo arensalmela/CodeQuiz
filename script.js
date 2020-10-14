c4f823f7f626373d3344462fe9d70e320491d749
Aren S <adsalmela@gmail.com>
2020-10-13 21:58:06 -0400

    added for loop to cycle through questions, get question function, and new timer.

--- script.js
+++ script.js
@@ -1,85 +1,122 @@
-//Variables for actual quiz.
-
-const question= document.getElementbyId("question");
-const choiceA= document.getElementbyId("A")
-const choiceB= document.getElementbyId("B")
-const choiceC= document.getElementbyId("C")
-const choiceD= document.getElementbyId("D")
-
-//Timer with Button
-
-document.addEventListener('DOMContentLoaded', () => {
-const timeLeftDisplay = document.querySelector ("#countDown")
-const startButton = document.querySelector("#timerButton")
-let timeLeft = 90
-
-    function timerCountDown(){
-        setInterval(function(){
-            if(timeLeft <= 0) {
-            clearInterval(timeLeft = 0)
-            }
-        timeLeftDisplay.innerHTML = timeLeft
-        timeLeft -=1
-        }, 1000)  
-    }
-
-startButton.addEventListener("click", timerCountDown)
-
-})
-
-
-
-let questions = [
+var questions = [
 
     {
         question: "Where do you link your external JS file within your HTML?",
-        choiceA: "Wrong",
-        choiceB: "Wrong",
-        choiceC: "Correct",
-        choiceD: "Wrong",
+        choices: ["answer1", "answer2", "answer3", "answer4"],
         correct: "C"
     },
 
     {
         question: "Where do you link your external JS file within your HTML?",
-        choiceA: "Wrong",
-        choiceB: "Correct",
-        choiceC: "Wrong",
-        choiceD: "Wrong",
-        correct: "B" 
+        choices: ["answer1", "answer2", "answer3", "answer4"],
+        correct: "B" //correct needs to be index or match string
     },
 
     {
         question: "Where do you link your external JS file within your HTML?",
-        choiceA: "Correct",
-        choiceB: "Wrong",
-        choiceC: "Wrong",
-        choiceD: "Wrong",
+        choices: ["answer1", "answer2", "answer3", "answer4"],
         correct: "A"
     },
 
     {
         question: "Where do you link your external JS file within your HTML?",
-        choiceA: "Wrong",
-        choiceB: "Wrong",
-        choiceC: "Correct",
-        choiceD: "Wrong",
+        choices: ["answer1", "answer2", "answer3", "answer4"],
         correct: "C"
     },
 ]
 
-const lastQuestion = questions.length - 1;
-let currentQuestion = 0;
 
-function callQuestion(){
-    let q = questions[currentQuestion];
 
-    question.innerHTML = "<p>"+ q.question +"</p>";
-    choiceA.innerHTML = q.choiceA;
-    choiceB.innerHTML = q.choiceB;
-    choiceC.innerHTML = q.choiceC;
-    choiceD.innerHTML = q.choiceD;
+
+
+var currentQuestionIndex = 0;
+var time = questions.length * 15
+var timerId; 
+var choicesEl = document.getElementById("choices") //button per choice
+var timerEl = document.getElementById("countDown")
+var feedbackEl = document.getElementById("feedback")
+var start = document.getElementById("start")
+//var questionEl = document.getElementById("quizSection")
+
+function getQuestion(){
+var currentQuestion = questions[currentQuestionIndex]
+var titleEl = document.getElementById("questionTitle")
+titleEl.textContent = currentQuestion.question
+
+for (let i = 0; i < currentQuestion.choices.length; i++) {
+    var choice = currentQuestion.choices[i]
+    var choiceNode = document.createElement("button")
+    choiceNode.setAttribute("class", "choice")
+    choiceNode.setAttribute("value", choice)
+    choiceNode.textContent = i + 1 + "." + choice
+    choiceNode.addEventListener("click", questionClick)    
+    choicesEl.appendChild(choiceNode)
+    console.log(choiceNode)
 
 }
+
+  
+
+}
+
+function questionClick(){
+    if (this.value !== question[currentQuestionIndex].correct){
+        time -=15
+        if (time<0){
+            time = 0
+        }
+      timerEl.textContent = time
+      feedbackEl.textContent = "Wrong" 
+
+    } else {
+        feedbackEl.textContent = "Correct"
+    }
+
+    feedbackEl.setAttribute("class", "feedback")
+    setTimeout(function(){
+        feedback.setAttribute("class", "feedback hide")
+    },1000)
+
+    currentQuestionIndex ++ 
+    if (currentQuestionIndex === questions.length){
+        quizEnd()
+    } else {
+        getQuestion()
+    }
+
+}
+
+function quizEnd(){
+clearInterval(timerId)
+var endScreenEl = document.getElementById("End-Screen")
+endScreenEl.removeAttribute("class")
+var finalScoreEl = document.getElementById("Final-Score")
+finalScoreEl.textContent = time
+//questionEl.setAttribute("class" , "hide")
+}
+
+start.addEventListener("click", getQuestion) 
+
+
+//Timer with Button
+
+// document.addEventListener('DOMContentLoaded', () => {
+// const timeLeftDisplay = document.querySelector ("#countDown")
+// const startButton = document.querySelector("#timerButton")
+// let timeLeft = 90
+
+//     function timerCountDown(){
+//         setInterval(function(){
+//             if(timeLeft <= 0) {
+//             clearInterval(timeLeft = 0)
+//             }
+//         timeLeftDisplay.innerHTML = timeLeft
+//         timeLeft -=1
+//         }, 1000)  
+//     }
+
+// startButton.addEventListener("click", timerCountDown)
+
+// })
 
 
